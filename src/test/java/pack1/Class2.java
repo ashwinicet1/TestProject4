4c4c99530fc568a423cfd73c9b3b0238d61aa214
ashwinicet1 <ashwinicet1@gmail.com>
2025-11-09 19:58:37 +0530

    Create Class2.java

---
+++ src/test/java/pack1/Class2.java
@@ -0,0 +1,15 @@
+
+package pack1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World today ddd");
+		
+		
+	}
+
+}
